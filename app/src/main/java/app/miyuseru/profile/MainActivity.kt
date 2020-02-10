99411d89f10f2fae80fe5e8e2ef57362ac03cf0d
miyuu kuwana <miyuu13k@gmail.com>
2020-02-10 16:14:56 +0900

    自己紹介アプり

---
+++ app/src/main/java/app/miyuseru/profile/MainActivity.kt
@@ -0,0 +1,27 @@
+package app.miyuseru.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.isVisible
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        //コメント〜〜
+        profileImage.isVisible = false
+        profileCommentText.isVisible = false
+
+        button1.setOnClickListener{
+            profileImage.isVisible = true
+            profileCommentText.isVisible = true
+
+
+        }
+
+    }
+}
